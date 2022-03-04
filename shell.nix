4bc7048ba18c3878896cc6ad2f3ff4cfd5dc7395
Devyani Jivani <devyanigjivani@gmail.com>
2022-03-04 11:11:39 -0500

    nix-shell update

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@
 #
 
 {
-  tag ? "21.11",
+  tag ? "21.05",
   graspiVersion ? "59f6a8a2e1ca7c8744a4e37701b919131efb2f45",
   pymksVersion ? "5aeb56c9faff8a655136747faa744b81d9549e3d"
 }:
@@ -21,10 +21,10 @@ let
 in
   pkgs.mkShell rec {
     pname = "pygraspi-env";
-    nativeBuildInputs = with pypkgs; [
+    buildInputs = with pypkgs; [
       pymks
-      graspi
-      jupyter
+#      graspi
+#      jupyter
     ];
 
     shellHook = ''
