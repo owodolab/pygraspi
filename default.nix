77fdc0857f3f091cc6dccd55cba7f857da5d75c7
Daniel Wheeler <daniel.wheeler2@gmail.com>
2022-05-31 12:01:45 -0400

    build(nix): add pymks as a dependency
    PyMKS is now a dependency of PyGraspi. PyMKS is used in the example
    notebooks to create data.

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@
 , scikitimage
 , sknw
 , nbval
+, pymks
 }:
 
 buildPythonPackage rec {
@@ -21,6 +22,7 @@ buildPythonPackage rec {
     scikitimage
     sknw
     nbval
+    pymks
   ];
 
   checkInputs = [ pytest ];
